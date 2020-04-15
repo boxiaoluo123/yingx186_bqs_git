3c6dd4a2321e784bfd466dabc0741115a074d547
baoqs <871115422@qq.comm>
2020-04-15 13:48:20 +0800

    这是应学项目第一个版本 v1.0

---
+++ src/main/java/com/baizhi/yingx186_bqs_git/Yingx186BqsGitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.yingx186_bqs_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yingx186BqsGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yingx186BqsGitApplication.class, args);
+    }
+
+}
