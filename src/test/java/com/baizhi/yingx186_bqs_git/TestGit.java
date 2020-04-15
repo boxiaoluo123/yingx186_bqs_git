2d79844f25ac8253ff06601cd243dbbd1041c395
baoqs <871115422@qq.comm>
2020-04-15 14:06:01 +0800

    测试类做了修改

--- src/test/java/com/baizhi/yingx186_bqs_git/TestGit.java
+++ src/test/java/com/baizhi/yingx186_bqs_git/TestGit.java
@@ -3,5 +3,6 @@ package com.baizhi.yingx186_bqs_git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("===");
+        System.out.println("---");
     }
 }
